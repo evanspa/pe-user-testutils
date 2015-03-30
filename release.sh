ab1027e2146f253b27b876c4b47b81ca7a792acf
Paul Evans <evansp2@gmail.com>
2015-03-30 11:13:16 -0400

    Updated release.sh

--- release.sh
+++ release.sh
@@ -6,7 +6,10 @@ lein release :patch
 git checkout tags/${NEXT_TAG}
 lein doc
 git checkout -b gh-pages
-mv -f doc/*.html .
-git commit -am "updated docs"
+mv -f doc/* .
+git add *.html
+git add css
+git add js
+git commit -m "updated docs"
 git push --set-upstream origin gh-pages
 git checkout master
